21550154e225985bb92d0311f71a2c31b4ad4fcc
mustafa12332 <abudalumustafa@gmail.com>
2017-06-24 19:22:24 +0300

    port fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 process.title = 'node-chat';
 
 // Port where we'll run the websocket server
-var webSocketsServerPort = process.env.PORT|1337;
+var webSocketsServerPort = 8080;
 
 // websocket and http servers
 var webSocketServer = require('websocket').server;
